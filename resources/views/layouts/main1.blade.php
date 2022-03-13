eb20e9e77a373f63ba6354746204722d9e2154ef
Bagus Miftah Nur Haqqi <bagus.miftah@outlook.com>
2022-03-13 22:44:09 +0700

    perbaikan favicon

--- resources/views/layouts/main1.blade.php
+++ resources/views/layouts/main1.blade.php
@@ -10,9 +10,9 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
     
     {{-- favicon --}}
-    <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png">
     <link rel="manifest" href="/site.webmanifest">  
 
     <!-- Bootstrap CSS -->
