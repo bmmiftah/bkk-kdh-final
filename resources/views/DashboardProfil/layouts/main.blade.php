f79873278a0a5e7b6adc0faa98cd3a1f477275fd
Bagus Miftah Nur Haqqi <bagus.miftah@outlook.com>
2022-03-13 22:29:37 +0700

    favicon profil

--- resources/views/DashboardProfil/layouts/main.blade.php
+++ resources/views/DashboardProfil/layouts/main.blade.php
@@ -10,6 +10,12 @@
     <meta name="author" content="">
 
     <title>My Profil | {{ $title }} </title>
+    
+    {{-- favicon --}}
+    <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">  
 
     <!-- Custom fonts for this template-->
     <link href="/dashboard/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
