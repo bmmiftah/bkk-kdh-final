be4e04d2001bf6b9c33ad50f30ed422501c584d3
Bagus Miftah Nur Haqqi <bagus.miftah@outlook.com>
2022-03-13 22:30:36 +0700

    favicon dashboard admin

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -17,6 +17,12 @@
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
+    {{-- favicon --}}
+    <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">  
+
     <!-- Custom styles for this template-->
     <link href="/dashboard/css/sb-admin-2.min.css" rel="stylesheet">
 
