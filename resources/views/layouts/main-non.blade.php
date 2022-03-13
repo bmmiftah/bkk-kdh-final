4f380d026b54c2bebfacbe16855141787d9564bf
Bagus Miftah Nur Haqqi <bagus.miftah@outlook.com>
2022-03-13 22:25:08 +0700

    favicon main-non blade

--- resources/views/layouts/main-non.blade.php
+++ resources/views/layouts/main-non.blade.php
@@ -6,6 +6,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    {{-- favicon --}}
+    <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">  
+    
      {{-- bootsraps icons --}}
      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
      
